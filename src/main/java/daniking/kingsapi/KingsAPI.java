5b7b7bbc659b7ca9f3b46be952b1ccb89b6d9b9e
danikingrd <danikingrd@gmail.com>
2021-12-18 17:20:24 -0400

    Setup

---
+++ src/main/java/daniking/kingsapi/KingsAPI.java
@@ -0,0 +1,16 @@
+package daniking.kingsapi;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * Daniking's API for Minecraft mods
+ */
+public class KingsAPI implements ModInitializer {
+
+    public static final String MOD_ID = "kingsapi";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
